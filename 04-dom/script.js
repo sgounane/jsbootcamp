ac1cb8760be639a73c384338fba23d8a9fc85f64
gounane <gounane.said@gmail.com>
2023-02-25 15:44:46 +0100

    exercice

--- 04-dom/script.js
+++ 04-dom/script.js
@@ -1,4 +1,5 @@
 let root=document.querySelector("#root")
+
 let students=[
     {
         id:1,
@@ -25,32 +26,35 @@ let students=[
         age: 22,
     },
     {
-        id:1,
+        id:5,
         nom: "yhay",
         prenom:"yahya",
         age: 24,
     },
     {
-        id:2,
+        id:6,
         nom: "Mouradi",
         prenom:"said",
         age: 21,
     },
     {
-        id:3,
+        id:7,
         nom: "nouri",
         prenom:"mourad",
         age: 24,
     },
     {
-        id:4,
+        id:8,
         nom: "achraf",
         prenom:"achraf",
         age: 22,
     },
 
 ]
+
+let name=prompt("nom: ")
 let str=""
+
 
 for( let s of students)
     str+=`<div class="box">
@@ -59,3 +63,12 @@ for( let s of students)
             <p>Age: ${s.age}</p>
         </div>`
 root.innerHTML=str
+
+let boxs=document.querySelectorAll(".box")
+console.log(boxs)
+students.forEach((e)=>{
+    if(e.nom.includes(name)){
+        console.log(e.id)
+        boxs[e.id-1].classList.add("selected")
+    }
+})
